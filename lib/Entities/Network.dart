033c6b59f02a6b983f89e1b6bef8cddfa3b93277
Moritz <38078338+MoritzJoekel@users.noreply.github.com>
2020-04-26 15:48:29 +0200

    Basisklassen implementiert. See #4

---
+++ lib/Entities/Network.dart
@@ -0,0 +1,23 @@
+class Network {
+  String interfaceName;
+
+  double timeSinceUpdate;
+
+  double cumulativeReceive;
+
+  double receive;
+
+  double cumulativeTx;
+
+  double tx;
+
+  double cumulativeCx;
+
+  double cx;
+
+  bool isUp;
+
+  double speed;
+
+  String key;
+}
--- lib/Entities/Sensor.dart
+++ lib/Entities/Sensor.dart
@@ -1,14 +1,17 @@
 import "Unit.dart";
 
 /// Represents values from Glances-API for Sensors.
-class Sensor
-{
+class Sensor {
+  /// Name of the specified sensor.
   String label;
 
+  /// Value of the specified sensor.
   double value;
 
+  /// Unit of the value.
   Unit unit;
 
+  /// Type of the sensor.
   String type;
 
   String key;

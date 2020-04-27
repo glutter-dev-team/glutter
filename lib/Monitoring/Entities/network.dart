57d6040fb4a3d05addc2420a64a2ba44508b78d6
Moritz <38078338+MoritzJoekel@users.noreply.github.com>
2020-04-27 21:03:47 +0200

    - Klasse für Profil erstellt closes #5 - Basisklassen und Entitäten erstellt closes #4 - GlancesService erstellt closes #3

--- lib/Monitoring/Entities/network.dart
+++ lib/Monitoring/Entities/network.dart
@@ -20,4 +20,34 @@ class Network {
   double speed;
 
   String key;
+
+  /// Constructor for Network-Objects.
+  Network(String interfaceName, double timeSinceUpdate, double cumulativeReceive, double receive, double cumulativeTx, double tx, double cumulativeCx, double cx, bool isUp, double speed, String key) {
+    this.interfaceName = interfaceName;
+    this.timeSinceUpdate = timeSinceUpdate;
+    this.cumulativeReceive = cumulativeReceive;
+    this.receive = receive;
+    this.cumulativeTx = cumulativeTx;
+    this.cx = cx;
+    this.isUp = isUp;
+    this.speed = speed;
+    this.key = key;
+  }
+
+  /// For deserialization of JSON and conversion to Network-Object.
+  factory Network.fromJson(dynamic json) {
+    return Network(
+        json['interface_name'] as String,
+        json['time_since_update'] as double,
+        json['cumulative_rx'] as double,
+        json['rx'] as double,
+        json['cumulative_tx'] as double,
+        json['tx'] as double,
+        json['cumulative_cx'] as double,
+        json['cx'] as double,
+        json['is_up'] as bool,
+        json['speed'] as double,
+        json['key'] as String
+    );
+  }
 }

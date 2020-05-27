952359714f93f8e0e58ea886c59f3be350f4930e
Moritz <38078338+MoritzJoekel@users.noreply.github.com>
2020-05-27 17:02:11 +0200

    Settings-Tabelle ergänzt und Database-Service erweitert. Dazu eine Werkstatt-Version vom Dashboard. See #32 und #37 Achtung: Die APP muss auf dem Gerät neu installiert werden!

--- lib/services/monitoring/database_provider.dart
+++ lib/services/monitoring/database_provider.dart
@@ -1,7 +1,7 @@
 /// Provides functions and constants for work with the database.
 class DatabaseProvider {
-    /// Provides the SQL-Script for OnCreate-Event of the database.
-    static String getOnCreate() {
+    /// Provides the SQL-Script for creating the Profiles-Table.
+    static String createProfileTable() {
         return
             "CREATE TABLE profiles ( " +
                 "id	INTEGER NOT NULL PRIMARY KEY, " +
@@ -9,6 +9,18 @@ class DatabaseProvider {
                 "port TEXT NOT NULL, " +
                 "glancesApiVersion TEXT NOT NULL, " +
                 "caption TEXT" +
-            ");";
+            ");"
+            ;
+    }
+
+    /// Provides the SQL-Script for creating the Profiles-Table.
+    static String createSettingsTable() {
+        return
+            "CREATE TABLE settings ( " +
+                "id	INTEGER NOT NULL PRIMARY KEY, " +
+                "defaultProfileId INTEGER NOT NULL, " +
+                "isDarkmode INTEGER NOT NULL" +
+                ");"
+        ;
     }
 }

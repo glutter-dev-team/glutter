550ed7c769d2f65420deba52470072737a5e0ab1
Moritz Jökel <38078338+MrzJkl@users.noreply.github.com>
2020-09-15 16:47:57 +0200

    #66 Kommentar korrigiert.

--- test/preferences_service_test.dart
+++ test/preferences_service_test.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:glutter/services/shared/preferences_service.dart';
 
 void main() {
-  /// Tests the correct functionality of getting the CPU-Values from the GlancesService.
+  /// Tests functionality of PreferenceService
   test('Preferences Service set/get', () async {
     Map<String, dynamic> values = {'defaultProfileId': 2};
     SharedPreferences.setMockInitialValues(values);

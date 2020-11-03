4fe5c072544f2733b06172543e91eeaeb1a21d21
HeLau1337 <hendrik.laudemann+git@gmail.com>
2020-11-03 17:27:30 +0100

    updated code for showing SnackBars (reason: used deprecated flutter-function)

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,13 +3,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 
-void showToast(BuildContext context, String message) {
-    final scaffold = Scaffold.of(context);
-    scaffold.showSnackBar(
+void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
             content: Text(message),
             action: SnackBarAction(
-                label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+                label: 'OK',
+                onPressed: ScaffoldMessenger.of(context).hideCurrentSnackBar),
         ),
     );
 }
@@ -28,9 +28,7 @@ String convertBytes(int bytes, int decimals) {
     if (bytes <= 0) return "0 B";
     const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
     var i = (log(bytes) / log(1024)).floor();
-    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) +
-        ' ' +
-        suffixes[i];
+    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) + ' ' + suffixes[i];
 }
 
 
